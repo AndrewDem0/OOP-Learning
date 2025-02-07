d580c976a8a17c969a17040ee3076d636861a5f0
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-02-07 18:53:31 +0200

    lab 1 done

--- Lab_1/Triangle.cpp
+++ Lab_1/Triangle.cpp
@@ -14,16 +14,10 @@ public:
             this->b = b;
             this->c = c;
             list_of_items.push_back(this);
-            std::cout << "\n Triangle " << display_triangle() << " append successfully" << std::endl;
+            std::cout << "\n Triangle " << *this << " append successfully" << std::endl;
         } else {
             std::cout << "\n This triangle cannot exist" << std::endl;
         }
-    }
-
-    void scale(double x) {
-        a *= x;
-        b *= x;
-        c *= x;
     }
 
     double perimeter() const { // const - метод не може змінювати дані класу крім mutable
@@ -35,13 +29,38 @@ public:
         return std::sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
-    std::string display_triangle() const {
-        return "/_\\ (" + std::to_string(a) + "; " + std::to_string(b) + "; " + std::to_string(c) + ")";
+    // Оператор виводу
+    friend std::ostream& operator<<(std::ostream& os, const Triangle& t) { // friend - дозволяє функції мати доступ до приватних членів класу
+        os << "/_\\ (" << t.a << "; " << t.b << "; " << t.c << ")";
+        return os;
+    }
+
+    // Оператори порівняння
+    bool operator > (const Triangle& other) const {
+        return this->square() > other.square();
+    }
+
+    bool operator < (const Triangle& other) const {
+        return this->square() < other.square();
+    }
+
+    // Оператор множення
+    Triangle operator * (int x) {
+        a = a * x;
+        b = b * x;
+        c = c * x;
+    }
+
+    // Оператор ділення
+    Triangle operator / (int x) {
+        a = a / x;
+        b = b / x;
+        c = c / x;
     }
 
     static void display_all_triangles() {
         for (const auto& t : list_of_items) { // auto& дає посилання на об'єкт вектора
-            std::cout << " /_\\ (" << t->a << "; " << t->b << "; " << t->c << ")" << std::endl;
+            std::cout << *t << "\n";
         }
     }
 
@@ -49,21 +68,28 @@ private:
     double a, b, c;
 };
 
+// Ініціалізація статичного вектора
 std::vector<Triangle*> Triangle::list_of_items;
 
 int main() {
     Triangle t1(3, 4, 5);
-    Triangle t2(1, 2, 3);
+    Triangle t2(5, 12, 13);
     Triangle t3(6, 8, 10);
 
-    std::cout << "\n Triangle t1 = " << t1.display_triangle() << ", perimeter = " << t1.perimeter() << ", square = " << t1.square() << std::endl;
-    std::cout << "\n Triangle t3 = " << t3.display_triangle() << ", perimeter = " << t3.perimeter() << ", square = " << t3.square() << std::endl;
+    std::cout << "t1: " << t1 << ", Perimeter: " << t1.perimeter() << ", Square: " << t1.square() << "\n";
+    std::cout << "t2: " << t2 << ", Perimeter: " << t2.perimeter() << ", Square: " << t2.square() << "\n";
+    std::cout << "t3: " << t3 << ", Perimeter: " << t3.perimeter() << ", Square: " << t3.square() << "\n";
 
-    std::cout << "\n Triangle t1 before scale: " << t1.display_triangle() << std::endl;
-    t1.scale(3);
-    std::cout << "\n Triangle t1 after scale: " << t1.display_triangle() << std::endl;
+    std::cout << "t1 > t2: " << (t1 > t2 ? "True" : "False") << "\n";
+    std::cout << "t1 < t3: " << (t1 < t3 ? "True" : "False") << "\n";
 
-    std::cout << "\n There are all Triangles ↓" << std::endl;
+     t1 * 2;
+    std::cout << "After scaling t1 * 2: " << t1 << "\n";
+
+     t1 / 2;
+    std::cout << "After scaling t1 / 2: " << t1<< "\n";
+
+    std::cout << "All triangles:\n";
     Triangle::display_all_triangles();
 
     return 0;
